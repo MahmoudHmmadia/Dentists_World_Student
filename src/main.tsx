5a564af90edb710b58d95e33c516865318130713
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-06-27 19:49:19 +0300

    fix issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "./context/Context";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-if (process.env.NODE_ENV === "production") {
-  disableReactDevTools();
-}
+disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
